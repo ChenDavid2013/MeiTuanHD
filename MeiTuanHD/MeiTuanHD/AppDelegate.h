7aa598e5c0c285795e4dbaefd8855781db88905c
ChenDavid2013 <375167422@qq.com>
2016-02-21 15:53:02 +0800

    初始化美团项目

---
+++ MeiTuanHD/MeiTuanHD/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MeiTuanHD
+//
+//  Created by 有何不可 on 16/2/21.
+//  Copyright © 2016年 有何不可. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
