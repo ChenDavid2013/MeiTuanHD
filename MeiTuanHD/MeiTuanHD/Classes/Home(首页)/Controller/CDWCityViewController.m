93d2602e818ab000e157af17efb58aab07d064d7
ChenDavid2013 <375167422@qq.com>
2016-02-24 11:37:51 +0800

    searchBar代理方法的简单实现

--- MeiTuanHD/MeiTuanHD/Classes/Home(首页)/Controller/CDWCityViewController.m
+++ MeiTuanHD/MeiTuanHD/Classes/Home(首页)/Controller/CDWCityViewController.m
@@ -8,7 +8,7 @@
 
 #import "CDWCityViewController.h"
 
-@interface CDWCityViewController ()<UITableViewDataSource, UITableViewDelegate>
+@interface CDWCityViewController ()<UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate>
 
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 @property (weak, nonatomic) IBOutlet UISearchBar *searchBar;
@@ -31,7 +31,9 @@ static NSString *ID = @"city";
                                              icon:@"btn_navigation_close"
                                              andHLIcon:@"btn_navigation_close_hl"];
     
-    [self.tableView registerNib:[UINib nibWithNibName:@"CDWCityViewController" bundle:nil] forCellReuseIdentifier:ID];
+    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:ID];
+    
+    self.searchBar.tintColor = CDWColor(28, 182, 165);
     
 }
 
@@ -53,6 +55,56 @@ static NSString *ID = @"city";
     return cell;
 }
 
+#pragma mark --searchBar的代理
+// MARK: --开始编辑的时候的操作
+- (void)searchBarTextDidBeginEditing:(UISearchBar *)searchBar {
+ 
+    [UIView animateWithDuration:0.25 animations:^{
+        
+        self.navigationController.navigationBarHidden = YES;
+        
+        self.searchBar.showsCancelButton = YES;
+        
+        self.searchBar.backgroundImage = [UIImage imageNamed:@"bg_login_textfield_hl"];
+    }];
+     
+    for (UIView *view in searchBar.subviews.firstObject.subviews) {
+        
+        if ([view isKindOfClass:[UIButton class]]) {
+            
+            [(UIButton *)view setTitle:@"取消" forState:UIControlStateNormal];
+            continue;
+        }
+    }
+    
+}
+// MARK: --结束编辑的时候的操作
+- (void)searchBarTextDidEndEditing:(UISearchBar *)searchBar {
+    
+    [UIView animateWithDuration:0.25 animations:^{
+        
+        self.navigationController.navigationBarHidden = NO;
+        
+        self.searchBar.showsCancelButton = NO;
+        
+        self.searchBar.backgroundImage = [UIImage imageNamed:@"bg_login_textfield"];
 
+    }];
+    
+}
+
+// MARK: --当输入文本就开始查询
+- (void)searchBar:(UISearchBar *)searchBar textDidChange:(NSString *)searchText {
+    NSLog(@"%s",__func__);
+
+    
+}
+
+// MARK: --点击取消按钮的操作
+- (void)searchBarCancelButtonClicked:(UISearchBar *)searchBar {
+    
+    NSLog(@"%s",__func__);
+    [searchBar resignFirstResponder];
+}
 
 @end
